7aefd9a22f8feca159e6b9de4da63325b236a640
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-30 02:10:58 +0300

    Assignment completed

--- main.js
+++ main.js
@@ -2,7 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Step 1: Hide the error modal
+  const errorModal = document.querySelector('#modal');
+  errorModal.classList.add('hidden');
+
+  // Step 2: Set up event listener for heart icons
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', handleHeartClick);
+  });
+});
+
+// Step 3: Handle click event on heart icons
+function handleHeartClick(event) {
+  const heart = event.target;
+  
+  // Step 3.1: Simulate server request
+  mimicServerCall()
+    .then(() => {
+      // Step 3.2: Handle successful response
+      heart.textContent = FULL_HEART;
+      heart.classList.add('activated-heart');
+    })
+    .catch((error) => {
+      // Step 3.3: Handle error response
+      const errorModal = document.querySelector('#modal');
+      const errorMessage = document.querySelector('#modal-message');
+      errorModal.classList.remove('hidden');
+      errorMessage.textContent = error;
+
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
 
 
 
